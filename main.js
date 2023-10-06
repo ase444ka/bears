1b1e29cd3910885769062c4125796de562b29bef
Ase444ka <istomina.asia@yandex.ru>
2023-10-06 18:19:01 +0300

    implement buttons

--- main.js
+++ main.js
@@ -2,23 +2,16 @@ import './style.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import './ui-kit/button.scss'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+
+document.querySelector('#app').innerHTML = /* jsx */`
+  <div class="buttons-demo">
+    <button class="button button_yes">Принять</button>
+    <button class="button button_no">Отклонить</button>
+    <button class="button button_no button_border_white">Отклонить</button>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+
+console.log('kkk', modules);
