47f65b576e9eee66e5d272841a4197053814ba95
Ase444ka <istomina.asia@yandex.ru>
2023-10-13 18:31:34 +0300

    layout cards

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import {setupCounter} from './counter.js';
 import './ui-kit/button.scss';
 import './ui-kit/checkbox.scss';
 import './ui-kit/close.scss';
+import './ui-kit/card.scss';
 
 document.querySelector('#app').innerHTML = /* jsx */ `
   <div class="buttons-demo">
@@ -37,5 +38,36 @@ document.querySelector('#app').innerHTML = /* jsx */ `
       <svg class="close__icon"><use href="./public/sprites.svg#cross"></use></svg>
     </button>
   </div>
+  <div class="cards-demo">
+  <div class="card">            
+    <img class="card__image" alt="бурый медведь" src="./public/images/bear1.png">
+    <div class="card__reserved-label">В заповеднике</div>
+    <h5 class="card__title">Донна</h5>
+    <p class="card__description"><span>Бурый медведь</span><span>Самец</span></p>
+    <div class="card__actions">
+      <button class="button button_yes">Принять</button>
+      <button class="button button_no">Отклонить</button>
+    </div>
+  </div>
+  <div class="card">            
+    <img class="card__image" alt="бурый медведь" src="./public/images/bear1.png">
+    <div class="card__reserved-label">В заповеднике</div>
+    <h5 class="card__title">Донна</h5>
+    <p class="card__description"><span>Бурый медведь</span><span>Самец</span></p>
+    <div class="card__actions">
+      <button class="button button_yes">Принять</button>
+      <button class="button button_no">Отклонить</button>
+    </div>
+  </div>
+  <div class="card card_reserved">            
+    <img class="card__image" alt="бурый медведь" src="./public/images/bear1.png">
+    <div class="card__reserved-label">В заповеднике</div>
+    <h5 class="card__title">Донна</h5>
+    <p class="card__description"><span>Бурый медведь</span><span>Самец</span></p>
+    <div class="card__actions">
+      <button class="button button_yes">Принять</button>
+      <button class="button button_no">Отклонить</button>
+    </div>
+  </div>
+</div>
 `;
-
